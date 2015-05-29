4c4645ecd3af162fc6200a510777b6f23f50edd6
Wensh88 <wenjiezhong@me.com>
2015-05-29 12:34:47 +0200

    Initial commit from local git

---
+++ Tweet Novelty Detection/main.swift
@@ -0,0 +1,14 @@
+//  main.swift
+//  Swift Tweets
+//
+//  Created for tweet expressions by Wenjie Zhong on 18-02-15
+
+import Foundation
+
+//Patterns
+let urlPattern = "(http:\\/\\/t.co\\/)[a-zA-Z0-9\\-\\.]{1,16}"
+let handlePattern = "([@])\\w+"
+let hashtagPattern = "\\B#\\w+"
+let retweetPattern = "\\bRT\\b"
+let retweetWithCommentPattern = "\\040RT\\b"
+
