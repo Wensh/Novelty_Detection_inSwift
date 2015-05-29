afbb173202775a7446c26f06561c577651aa7fc5
Wensh88 <wenjiezhong@me.com>
2015-05-29 12:32:03 +0200

    Initial Commit

---
+++ Tweet Novelty DetectionTests/Tweet_Novelty_DetectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tweet_Novelty_DetectionTests.swift
+//  Tweet Novelty DetectionTests
+//
+//  Created by Wenjie Zhong on 29-05-15.
+//  Copyright (c) 2015 Wenjie Zhong. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Tweet_Novelty_DetectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
