e3d5d36e88b876005262daf2b3c5e8508576f960
Wensh88 <wenjiezhong@me.com>
2015-05-29 14:11:37 +0200

    Merged with previous tweets project.

---
+++ Tweet Novelty Detection/Levenshteins Distance.swift
@@ -0,0 +1,40 @@
+// memoized Levenshtein Distance
+
+import Foundation
+
+// memoize for a two parameter recursive function
+func memoize<T1: Hashable, T2: Hashable, U>(body: ((T1, T2) -> U, T1, T2) -> U) -> ((T1, T2) -> U) {
+    var memo = [T1: [T2: U]]()
+    var result: ((T1, T2) -> U)!
+    result = {
+        (value1: T1, value2: T2) -> U in
+        if let cached = memo[value1]?[value2] { return cached }
+        
+        let toCache = body(result, value1, value2)
+        if memo[value1] == nil { memo[value1] = [:] }
+        memo[value1]![value2] = toCache
+        return toCache
+    }
+    return result
+}
+
+let levenshteinDistance = memoize {
+    (levenshteinDistance: (String, String) -> Int, s1: String, s2: String) -> Int in
+    if isEmpty(s1) { return count(s2) }
+    if isEmpty(s2) { return count(s1) }
+    
+    // drop first letter of each string
+    let s1Crop = s1[s1.startIndex.successor()..<s1.endIndex]
+    let s2Crop = s2[s2.startIndex.successor()..<s2.endIndex]
+    
+    // if first characters are equal, continue with both cropped
+    if s1[s1.startIndex] == s2[s2.startIndex] {
+        return levenshteinDistance(s1Crop, s2Crop)
+    }
+    
+    // otherwise find smallest of the three options
+    let (c1, c2, c3) = (levenshteinDistance(s1Crop, s2), levenshteinDistance(s1, s2Crop), levenshteinDistance(s1Crop, s2Crop))
+    return 1 + min(min(c1, c2), c3)
+}
+
+//levenshteinDistance("hill", "hilt")
--- Tweet Novelty Detection/main.swift
+++ Tweet Novelty Detection/main.swift
@@ -5,6 +5,13 @@
 
 import Foundation
 
+//Input
+func urlForScene() -> NSURL {
+    let filepath = "/Users/wenjiezhong/Dropbox/AI/CrowdTruth/Curation/TweetStream.csv"
+    var filename:NSString = "Whaling"
+    return NSURL.fileURLWithPath(filepath)!
+}
+
 //Patterns
 let urlPattern = "(http:\\/\\/t.co\\/)[a-zA-Z0-9\\-\\.]{1,16}"
 let handlePattern = "([@])\\w+"
@@ -12,4 +19,57 @@ let hashtagPattern = "\\B#\\w+"
 let retweetPattern = "\\bRT\\b"
 let retweetWithCommentPattern = "\\040RT\\b"
 
-//testing commit
+//Output
+var columnOfTweets = [String]()
+var handleOutput : [String] = [""]
+
+func generateTweets() {
+    var tweet = [Int: String]()
+    let url = urlForScene()
+    var error: NSErrorPointer = nil
+    if let csv = CSV(contentsOfURL: url, error: error) {
+        // Rows
+        let rows = csv.rows
+        let headers = csv.headers
+        
+        // Columns
+        let columns = csv.columns
+        columnOfTweets = columns["tweet"]!
+    }
+}
+
+generateTweets()
+
+/*
+while columnOfTweets.count < 2000 {
+    tweetLinkOutput[countLink] = ""
+    outputOfURL[countLink] = ""
+    tweetlets = columnOfTweets[indexOfColumns].componentsSeparatedByString(" ")
+    while indexOfTweetlets < tweetlets.count {
+        if (count(tweetlets[indexOfTweetlets]) >= 4) {
+            if (tweetlets[indexOfTweetlets].substringToIndex(startIndex) == "http" && count(tweetlets[indexOfTweetlets]) > 20) {
+                testURL = retrieveURL(tweetlets[indexOfTweetlets])
+                checkEmptyIndex = true
+                outputOfURL.insert("\(outputOfURL[countLink])\(testURL), ", atIndex: countLink)
+}
+if (count(testURL) >= 19 && testURL.substringToIndex(websiteIndex) == "https://www.youtub") {
+tweetLinkOutput.insert("\(tweetLinkOutput[countLink])Youtube, ", atIndex: countLink)
+}
+else if (count(testURL) >= 19 && testURL.substringToIndex(websiteIndex) == "https://twitter.co") {
+tweetLinkOutput.insert("\(tweetLinkOutput[countLink])Twitter, ", atIndex: countLink)
+}
+else {
+tweetLinkOutput.insert("\(tweetLinkOutput[countLink])News, ", atIndex: countLink)
+}
+*/
+
+/*
+let handlesFileName = "handles.csv"
+
+if let dirs : [String] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true) as? [String] {
+let dir = dirs[0] //documents directory
+
+let pathToWriteForHandles = dir.stringByAppendingPathComponent(handlesFileName)
+let joinedHandles = "\n".join(handleOutput)
+joinedHandles.writeToFile(pathToWriteForHandles, atomically:true, encoding:NSUTF8StringEncoding)
+*/
