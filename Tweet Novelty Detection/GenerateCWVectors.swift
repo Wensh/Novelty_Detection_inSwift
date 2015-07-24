c452b038c70e22b2598a12ffcbb21a6f7b0f7f0d
Wenjie Zhong <wenjiezhong@Zhong.local>
2015-07-24 04:21:06 +0200

    Added CrowdTruth vectors

---
+++ Tweet Novelty Detection/GenerateCWVectors.swift
@@ -0,0 +1,9 @@
+//
+//  GenerateCWVectors.swift
+//  Tweet Novelty Detection
+//
+//  Created by Wenjie Zhong on 24-07-15.
+//  Copyright (c) 2015 Wenjie Zhong. All rights reserved.
+//
+
+import Foundation
